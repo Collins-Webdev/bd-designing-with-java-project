82e771ead4745cae9d2751111d70828a14e7629e
Collins-Webdev <collinshaya@gmail.com>
2024-09-26 10:06:50 +0100

    test

--- src/com/amazon/ata/types/Box.java
+++ src/com/amazon/ata/types/Box.java
@@ -47,10 +47,10 @@ public class Box extends Packaging {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o){
+        if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()){
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
         Box box = (Box) o;
